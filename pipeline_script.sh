1db52d57992e446bb1b7e6329de3426d62a08fee
RAMANAIDU GUNTUPALLI <guntupalliramanaidu@outlook.com>
2023-09-25 10:51:11 +0000

    Updated pipeline_script.sh

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -19,7 +19,6 @@ echo "--------------------------------------------------------"
 Connection_String=$(az storage account show-connection-string --name $AccountName --resource-group $RG --query connectionString -o tsv)
 az storage blob upload-batch \
   -d "\$web" \
-  -s "website" \
   --connection-string $Connection_String \
   --overwrite \
   --pattern "*"
